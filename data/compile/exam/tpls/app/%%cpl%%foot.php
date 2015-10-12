0bb1d889ae55c2368e47284a2b09934db51369f8
Hamster Tian <haotia@gmail.com>
2015-10-12 12:44:19 +0800

    (BY) Initial version of NCRE Simulation
    * By @naihaishy

---
+++ data/compile/exam/tpls/app/%%cpl%%foot.php
@@ -0,0 +1,18 @@
+<div class="row-fluid">
+	<div class="container-fluid logfooter">
+		<ul class="inline unstyled"  style="font-size:90%">
+			<li><a href="index.php">网站首页</a></li>
+			<li>|</li>
+			<li><a href="http://www.hustca.com" target="_blank">华中科技大学计算机协会</a></li>
+			<li>|</li>
+			<li><a href="http://www.bbs.hustca.com" target="_blank">网站论坛</a></li>
+			<li>|</li>
+			<li><a href="http://contact.php" target="_blank">联系我们</a></li>
+			<br>
+			<li>©2015 HUST CA , HuaZhong University of Science and Technology | All rights reserved. </li>
+		</ul>
+	</div>
+</div>
+<script type="text/javascript">
+$(".thumbnails img").bind("error",function(){$(this).attr("src","app/core/styles/images/icons/Book.png?"+Math.random());});
+</script>
